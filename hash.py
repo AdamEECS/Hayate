03652d920635e539fcbe6381d5169758e318c7ac
Suzumiya <jrgwyz@163.com>
2016-12-30 14:49:59 +0800

    fixed

--- hash.py
+++ hash.py
@@ -6,8 +6,7 @@ pwd = '123'.encode('ascii')
 m = hashlib.md5(pwd)
 # 返回摘要字符串, 这里是 c9c1ebed56b2efee7844b4158905d845
 print(m.hexdigest())
-#
-import hashlib
+
 # 创建 sha1 对象
 s = hashlib.sha1(pwd)
 # 返回摘要字符串, 这里是 4843c628d74aa10769eb21b832f00a778db8b17e
